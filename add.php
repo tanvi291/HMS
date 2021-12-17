3769faf9a94433df0b62b9191cdd29c67ac5f067
tanvi291 <96291533+tanvi291@users.noreply.github.com>
2021-12-17 16:10:55 +0530

    Add files via upload

---
+++ add.php
@@ -0,0 +1,16 @@
+<?php
+				if(isset($_POST['sub']))
+				{
+					$name =$_POST['name'];
+					$phone = $_POST['phone'];
+					$email = $_POST['email'];
+					$approval = "Not Allowed";
+
+					$sql = "INSERT INTO `contact`(`fullname`, `phoneno`, `email`,`cdate`,`approval`) VALUES ('$name','$phone','$email',now(),'$approval')" ;
+					
+					
+					if(mysqli_query($con,$sql))
+					echo"OK";
+					
+				}
+?>
